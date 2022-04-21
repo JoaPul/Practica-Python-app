3647260b5cfe9d23ee169aa6a5b6784f33abdd42
Joaquin Arroyo <87846400+JoaPul@users.noreply.github.com>
2022-04-21 11:13:50 -0500

    Files added

---
+++ website/models.py
@@ -0,0 +1,20 @@
+from . import db
+from flask_login import UserMixin  # Help the user to login
+from sqlalchemy.sql import func
+
+# foreignkey refere a name of a column for another database
+
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    notes = db.relationship('Note')
